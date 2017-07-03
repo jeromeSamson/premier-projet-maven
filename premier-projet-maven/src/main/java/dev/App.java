815dda31208b97b7b20d80fef046a0db3f0ad20c
Jérôme SAMSON DIT LAVIGNE <jerome.samson07@gmail.com>
2017-07-03 16:56:12 +0200

    tp03 maven

--- premier-projet-maven/src/main/java/dev/App.java
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -1,12 +1,13 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( FigletFont.convertOneLine("Hello world!") );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println(FigletFont.convertOneLine(ResourceBundle.getBundle("application").getString("titre")));
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement : " + environnement);
+	}
 }
